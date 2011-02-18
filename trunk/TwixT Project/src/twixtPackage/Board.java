14ea9c4a5ea39775ce33bce4fc20be7f3a2cb1bc
gavinm1@tcd.ie <gavinm1@tcd.ie@7fb19570-3518-8153-c07a-fda716b8aad6>
2011-02-18 16:39:03 +0000

    Board Class: -Changed the getBridge functions

--- trunk/TwixT Project/src/twixtPackage/Board.java
+++ trunk/TwixT Project/src/twixtPackage/Board.java
@@ -1,11 +1,17 @@
 package twixtPackage;
 
+import java.util.Vector;
+
 
 public class Board {
 
 	static int BOARDSIZE = 24;
 	
 	Tower board[][];
+	
+	//TODO board needs an vector of bridges
+	Vector bridgeList = new Vector();
+	
 	
 	/**
 	 * Constructor for Board object<br>
@@ -21,8 +27,8 @@ public class Board {
 				board[i][j] = null;
 			}
 		}
-	}
-		
+	}		
+	
 	public void placeTower(int x, int y, int owner)
 	{
 		Tower tower = new Tower(x, y, owner);
@@ -39,7 +45,23 @@ public class Board {
 		return board[x][y];
 	}
 	
-	public Bridge getBridge(int x, int y)
+	/**
+	 * getBridge function returns a bridge between 2 sets of coordinates.<br>
+	 * If the Tower doesn't exist then <code>null</code> is returned.<br>
+	 * 
+	 * @param x1 The x coordinate of the first tower
+	 * @param y1 The y coordinate of the first tower
+	 * @param x2 The x coordinate of the second tower
+	 * @param y2 The y coordinate of the second tower
+	 */
+	public Bridge getBridge(int x1, int y1, int x2, int y2)
+	{
+		
+		// Needs to be looked at, incomplete
+		return Bridge;
+	}
+	
+	public Bridge getBridge(int x1, int y1, int x2, int y2, int id)
 	{
 		// Needs to be looked at, incomplete
 		return Bridge; 
