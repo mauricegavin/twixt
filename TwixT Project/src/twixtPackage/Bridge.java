b79ad6b78c26f07fd41267386248e56b58b74d4b
connelk@tcd.ie <connelk@tcd.ie@7fb19570-3518-8153-c07a-fda716b8aad6>
2011-02-18 16:34:54 +0000

    Bridge - finished ?

--- TwixT Project/src/twixtPackage/Bridge.java
+++ TwixT Project/src/twixtPackage/Bridge.java
@@ -23,7 +23,25 @@ public class Bridge {
 		
 	}
 	
-	
-	
+	Tower getStart(){
+		return T1;
 	}
+	Tower getEnd(){
+		return T2;
+	}
+	int getID(){
+		return playerId;
+	}
+	void setStart(Tower T){
+		T1 = T;
+	}
+	void setEnd(Tower T){
+		T2 = T;
+		}
+	void setPlayerID(int id){
+		assert(id>0&&id<3): "Error: value must be between 1 and 2 ";
+		playerId = id; 
+	}
+		
+	
 }
