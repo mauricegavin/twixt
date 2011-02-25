875272b384afec59d70081733898fa06e2a0a066
costigaj@tcd.ie <costigaj@tcd.ie@7fb19570-3518-8153-c07a-fda716b8aad6>
2011-02-25 15:54:36 +0000

    RuleMaster-- fixed more bugs in canPlaceBridge

--- trunk/TwixT Project/src/twixtPackage/TestMain.java
+++ trunk/TwixT Project/src/twixtPackage/TestMain.java
@@ -13,30 +13,26 @@ public class TestMain {
 	public static void main(String[] args) {
 		Board b = new Board();
 		RuleMaster rm = new RuleMaster(b);
-		System.out.println(rm.canPlaceTower(12, 12));
-		System.out.println(rm.canPlaceTower(14, 13));
-		b.placeTower(12, 12, 1);
-		b.placeTower(14, 13, 1);
-		b.placeTower(13, 14, 1);
-		b.placeTower(14, 12, 1);
-		if(b.getTower(12, 12)!=null){
-			System.out.println("Tower found placed at :"+b.getTower(12, 12).getX()+", "+b.getTower(12, 12).getY());
-		}
-		if(b.getTower(14, 13)!=null){
-			System.out.println("Tower found placed at :"+b.getTower(14, 13).getX()+", "+b.getTower(14, 13).getY());
-		}
-		testCanPlaceBridge(12,12,14,13,1,rm);
-		testPlaceBridge(12,12,14,13,1,b);
+		testPlaceTower(12,12,1,b);
+		testPlaceTower(13,14,1,b);
+		testPlaceTower(14,13,1,b);
 		testCanPlaceBridge(12,12,13,14,1,rm);
-		testPlaceBridge(12,12,13,14,1,b);
-		testCanPlaceBridge(13,14,14,12,1,rm);
-		//testPlaceBridge(13,14,14,12,1,b);
+		testCanPlaceBridge(12,12,14,13,1,rm);
 	}
 	private static void testCanPlaceBridge(int x1,int y1,int x2,int y2,int pID,RuleMaster rm){
 		System.out.println("---------------------------------------------");
 		System.out.println("Testing canPlaceBridge");
 		System.out.println("Can place "+pID+"bridge between "+x1+" "+y1+" "+x2+" "+y2+" :"+rm.canPlaceBridge(x1, y1, x2, y2, pID));
 		System.out.println("---------------------------------------------");
+	}
+	private static void testCanPlaceTower(int x, int y, int pID, RuleMaster rm){
+		System.out.println("Testing player "+pID+" canPlaceTower @:"+x+" "+y+" "+rm.canPlaceTower(x, y));
+	}
+	private static void testPlaceTower(int x, int y, int pID, Board b){
+		b.placeTower(x, y, pID);
+		if(b.getTower(x, y)!=null){
+			System.out.println("Tower found placed at :"+b.getTower(x, y).getX()+", "+b.getTower(x, y).getY());
+		}
 	}
 	private static void testPlaceBridge(int x1,int y1,int x2,int y2,int pID, Board b){
 		b.placeBridge(x1, y1, x2, y2, pID);
