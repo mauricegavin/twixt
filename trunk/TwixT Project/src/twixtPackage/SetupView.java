68cae9ee43928740f87c917ea282abc72de3d33b
gavinm1@tcd.ie <gavinm1@tcd.ie@7fb19570-3518-8153-c07a-fda716b8aad6>
2011-02-27 22:11:49 +0000

    SetupView Class: - Added the Player Type portion of the GUI

--- trunk/TwixT Project/src/twixtPackage/SetupView.java
+++ trunk/TwixT Project/src/twixtPackage/SetupView.java
@@ -13,7 +13,9 @@ import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
+import javax.swing.JSlider;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.WindowConstants;
 
@@ -52,7 +54,7 @@ public class SetupView
 		setupFrame.setSize(dimensionSuggested); // Apply the suggested size to the frame
 		setupFrame.setLocation((screenDimension.width - dimensionMin.width)/2, (screenDimension.height - dimensionMin.height)/4); // Set the location to the upper (Y), middle (X) portion of the screen.
 		
-		setupFrame.getContentPane().setLayout(new FlowLayout()); // Give the main frame a Border Layout  
+		setupFrame.getContentPane().setLayout(new BorderLayout()); // Give the main frame a Border Layout  
 
 					
 		//
@@ -71,15 +73,40 @@ public class SetupView
 		
 		JLabel headingLabel3 = new JLabel("Network Settings");
 		
-		JLabel gameTypeLabel1 = new JLabel("Play Locally");
-		JLabel gameTypeLabel2 = new JLabel("Play Online");
+		// Game Type Labels
+		JLabel gtpgLabel1 = new JLabel("Play Locally");
+		JLabel gtpgLabel2 = new JLabel("Play Online");
 		
-		JRadioButton gameTypeOption1 = new JRadioButton();
-		JRadioButton gameTypeOption2 = new JRadioButton();
+		// Player Type Labels
+		JLabel ptp1HeadingLabel = new JLabel("Player 1");
+		JLabel ptp1bLabel1 = new JLabel("Human");
+		JLabel ptp1bLabel2 = new JLabel("AI   ");
+		JLabel ptp2HeadingLabel = new JLabel("Player 2");
+		JLabel ptp2bLabel1 = new JLabel("Human");
+		JLabel ptp2bLabel2 = new JLabel("AI   ");
 		
-		//JLabel = new JLabel("");
+		// Radio Buttons.
+		JRadioButton gtpgRadio1 = new JRadioButton();
+		JRadioButton gtpgRadio2 = new JRadioButton();
+		JRadioButton ptp1bRadio1 = new JRadioButton();
+		JRadioButton ptp1bRadio2 = new JRadioButton();
+		JRadioButton ptp2bRadio1 = new JRadioButton();
+		JRadioButton ptp2bRadio2 = new JRadioButton();
 		
-
+		// Text Fields
+		JTextField ptp1bTextField = new JTextField(10);
+		JTextField ptp2bTextField = new JTextField(10);
+		
+		// Sliders
+		JSlider ptp1bDifficultySlider = new JSlider(0, 5, 0);
+		ptp1bDifficultySlider.setMajorTickSpacing(1);
+		ptp1bDifficultySlider.setPaintTicks(true);
+		ptp1bDifficultySlider.setSnapToTicks(true);
+		
+		JSlider ptp2bDifficultySlider = new JSlider(0, 5, 0);
+		ptp2bDifficultySlider.setMajorTickSpacing(1);
+		ptp2bDifficultySlider.setPaintTicks(true);
+		ptp2bDifficultySlider.setSnapToTicks(true);
 		
 		//
 		// Panels & Layout
@@ -107,27 +134,62 @@ public class SetupView
 		JPanel startGamePanel = new JPanel();
 		gameTypePanel.setLayout(new BorderLayout());
 		
-		// Game Type Panel Sub Panels
-		JPanel subGameTypeHeadingPanel = new JPanel();
-		JPanel subGameTypePanel1 = new JPanel();
-		subGameTypePanel1.setLayout(new FlowLayout());
-		JPanel subGameTypePanel2 = new JPanel();
-		subGameTypePanel2.setLayout(new FlowLayout());
+		// Game Type Panel (gtp)
+		JPanel gtpHeading = new JPanel();
+		gtpHeading.setLayout(new FlowLayout());
 		
-		// Player Type Panel Sub Panels
-		JPanel subPlayerTypeHeadingPanel = new JPanel();
-		JPanel subPlayerTypePanel1 = new JPanel();
-		subPlayerTypePanel1.setLayout(new FlowLayout());
+		JPanel gtpGames = new JPanel();
+		gtpGames.setLayout(new BoxLayout(gtpGames, BoxLayout.LINE_AXIS));
 			
-			// Sub Player Type Panel 1 Panels
-			JPanel subSubPlayerTypePanel1 = new JPanel();
-			subPlayerTypePanel1.setLayout(new FlowLayout());
-			JPanel subSubPlayerTypePanel1 = new JPanel();
-			subPlayerTypePanel1.setLayout(new FlowLayout());
+			// Game Type Panel Games (gtpg)
+			JPanel gtpgLabels = new JPanel();
+			gtpgLabels.setLayout(new BoxLayout(gtpgLabels, BoxLayout.PAGE_AXIS));
+			JPanel gtpgRadio = new JPanel();
+			gtpgRadio.setLayout(new BoxLayout(gtpgRadio, BoxLayout.PAGE_AXIS));
 		
+		// Player Type Panel (ptp)
+		JPanel ptpHeading = new JPanel();
+		ptpHeading.setLayout(new FlowLayout());
+		
+		JPanel ptpPlayer1 = new JPanel();
+		ptpPlayer1.setLayout(new FlowLayout());
+		
+			// Player 1 Panel (ptp1)
+			JPanel ptp1Heading = new JPanel();
+			ptp1Heading.setLayout(new FlowLayout());
+			JPanel ptp1Body = new JPanel();
+			ptp1Body.setLayout(new BoxLayout(ptp1Body, BoxLayout.LINE_AXIS));
+		
+				// Player 1 Body (ptp1b)
+				JPanel ptp1bLabels = new JPanel();
+				ptp1bLabels.setLayout(new BoxLayout(ptp1bLabels, BoxLayout.PAGE_AXIS));
+				JPanel ptp1bRadio = new JPanel();
+				ptp1bRadio.setLayout(new BoxLayout(ptp1bRadio, BoxLayout.PAGE_AXIS));
+				JPanel ptp1bOptions = new JPanel();
+				ptp1bOptions.setLayout(new BoxLayout(ptp1bOptions, BoxLayout.PAGE_AXIS));
+			
+		JPanel ptpPlayer2 = new JPanel();
+		ptpPlayer2.setLayout(new FlowLayout());
+			
+			// Player 2 Panel (ptp2)
+			JPanel ptp2Heading = new JPanel();
+			ptp2Heading.setLayout(new FlowLayout());
+			JPanel ptp2Body = new JPanel();
+			ptp2Body.setLayout(new BoxLayout(ptp2Body, BoxLayout.LINE_AXIS));
+
+				// Player 2 Body (ptp2b)
+				JPanel ptp2bLabels = new JPanel();
+				ptp2bLabels.setLayout(new BoxLayout(ptp2bLabels, BoxLayout.PAGE_AXIS));
+				JPanel ptp2bRadio = new JPanel();
+				ptp2bRadio.setLayout(new BoxLayout(ptp2bRadio, BoxLayout.PAGE_AXIS));
+				JPanel ptp2bOptions = new JPanel();
+				ptp2bOptions.setLayout(new BoxLayout(ptp2bOptions, BoxLayout.PAGE_AXIS));
+			
 		// Add Panels to the Frame
 
-		setupFrame.add(setupViewBox);
+		setupFrame.add(new JSeparator(SwingConstants.HORIZONTAL), BorderLayout.NORTH);
+		setupFrame.add(setupViewBox, BorderLayout.CENTER);
+		setupFrame.add(new JSeparator(SwingConstants.HORIZONTAL), BorderLayout.SOUTH);
 		
 		setupViewBox.add(gameTypePanel);
 		setupViewBox.add(new JSeparator(SwingConstants.VERTICAL));
@@ -137,40 +199,67 @@ public class SetupView
 		setupViewBox.add(new JSeparator(SwingConstants.VERTICAL));
 		setupViewBox.add(startGamePanel);
 		
-		gameTypePanel.add(subGameTypeHeadingPanel);		
-		gameTypePanel.add(subGameTypePanel1);
-		gameTypePanel.add(subGameTypePanel2);
+		gameTypePanel.add(gtpHeading);
+		gameTypePanel.add(new JSeparator(SwingConstants.HORIZONTAL));
 		
-		playerTypePanel.add(subPlayerTypeHeadingPanel);		
-		playerTypePanel.add(subPlayerTypePanel1);
-		playerTypePanel.add(subPlayerTypePanel2);
+		gameTypePanel.add(gtpGames);
+			gtpGames.add(gtpgLabels);
+			gtpGames.add(gtpgRadio);
+			
+		playerTypePanel.add(ptpHeading);
+		playerTypePanel.add(new JSeparator(SwingConstants.HORIZONTAL));
+		
+		playerTypePanel.add(ptpPlayer1);
+			ptpPlayer1.add(ptp1Heading);
+			ptpPlayer1.add(ptp1Body);
+				ptp1Body.add(ptp1bLabels);
+				ptp1Body.add(ptp1bRadio);
+				ptp1Body.add(ptp1bOptions);
+		playerTypePanel.add(new JSeparator(SwingConstants.HORIZONTAL));
+				
+		playerTypePanel.add(ptpPlayer2);
+			ptpPlayer2.add(ptp2Heading);
+			ptpPlayer2.add(ptp2Body);
+				ptp2Body.add(ptp2bLabels);
+				ptp2Body.add(ptp2bRadio);
+				ptp2Body.add(ptp2bOptions);
 		
 		// Add Components to Panels
 		
 		// Game Type Panel
-		subGameTypeHeadingPanel.add(headingLabel1);
-		subGameTypePanel1.add(gameTypeLabel1);
-		subGameTypePanel1.add(gameTypeOption1);
-		subGameTypePanel2.add(gameTypeLabel2);
-		subGameTypePanel2.add(gameTypeOption2);
+		gtpHeading.add(headingLabel1);
 		
+		gtpgLabels.add(gtpgLabel1);
+		gtpgLabels.add(gtpgLabel2);
+		gtpgRadio.add(gtpgRadio1);
+		gtpgRadio.add(gtpgRadio2);
+				
 		// Player Type Panel
-		subPlayerTypeHeadingPanel.add(headingLabel1);
-		subPlayerTypePanel1.add(gameTypeLabel1);
-		subPlayerTypePanel1.add(gameTypeOption1);
-		subPlayerTypePanel2.add(gameTypeLabel2);
-		subPlayerTypePanel2.add(gameTypeOption2);
+		ptpHeading.add(headingLabel2);
+		
+		ptp1Heading.add(ptp1HeadingLabel);
+		ptp1bLabels.add(ptp1bLabel1);
+		ptp1bLabels.add(ptp1bLabel2);
+		ptp1bRadio.add(ptp1bRadio1);
+		ptp1bRadio.add(ptp1bRadio2);
+		ptp1bOptions.add(ptp1bTextField);
+		ptp1bOptions.add(ptp1bDifficultySlider);
+		
+		ptp2Heading.add(ptp2HeadingLabel);
+		ptp2bLabels.add(ptp2bLabel1);
+		ptp2bLabels.add(ptp2bLabel2);
+		ptp2bRadio.add(ptp2bRadio1);
+		ptp2bRadio.add(ptp2bRadio2);
+		ptp2bOptions.add(ptp2bTextField);
+		ptp2bOptions.add(ptp2bDifficultySlider);		
+		
 		
 		// Network Options Panel
-		subGameTypeHeadingPanel.add(headingLabel1);
-		subGameTypePanel1.add(gameTypeLabel1);
-		subGameTypePanel1.add(gameTypeOption1);
-		subGameTypePanel2.add(gameTypeLabel2);
-		subGameTypePanel2.add(gameTypeOption2);
-
-		// Player Type Panel
-		playerTypePanel.add(headingLabel2);
-		
+		//subGameTypeHeadingPanel.add(headingLabiohel1);
+		//ptpPlayer1.add(gameTypeLabel1);
+		//ptpPlayer1.add(gameTypeOption1);
+		//ptpPlayer2.add(gameTypeLabel2);
+		//ptpPlayer2.add(gameTypeOption2);		
 		// Network Settings Panel
 		networkSettingsPanel.add(headingLabel3);
 		
