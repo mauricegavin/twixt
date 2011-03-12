fac627dd11a5d882c07c1fa0c2079bf4f06a178d
gavinm1@tcd.ie <gavinm1@tcd.ie@7fb19570-3518-8153-c07a-fda716b8aad6>
2011-03-12 22:46:18 +0000

    BoardView Class: - Added a boarder around the play area; - Added the respective player boarders; - Updated the 'paint' method; - It now draws the components from the Game object's board, namely towers and bridges.
    Game Class:
    - Renamed the 'createNewGameView' function to 'createNewGame'
    - Removed the RuleMaster and Board initialisation from the default constructor.
    
    SetupView Class:
    - Renamed the createNewGameView function in line with the Game class.
    
    GameView Class:
    - Tweaked the window minimum dimensions.

--- TwixT Project/src/twixtPackage/BoardView.java
+++ TwixT Project/src/twixtPackage/BoardView.java
@@ -1,10 +1,14 @@
 package twixtPackage;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.Stroke;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
 import java.awt.geom.Ellipse2D;
@@ -15,15 +19,20 @@ import javax.swing.JTextArea;
 
 public class BoardView extends JComponent
 {
-
+	Test test = new Test(true);
+	
 	public int side = 400;
 	private static final int RADIUS = 10;
+	private static final int SPACE = 10;
+	private int BOARDSIZE = 24;
 	
 	Color colourBackground = Color.WHITE;
 	Color colourDefault = Color.GRAY;
+	Color colourBorder = Color.BLACK;
 	Color colourPlayer1 = Color.RED;
 	Color colourPlayer2 = Color.BLUE;
 
+	
 	int startingOffsetX = 25;
 	int startingOffsetY = 25;
 	int offsetX = 0;
@@ -31,12 +40,16 @@ public class BoardView extends JComponent
 	int componentWidth = 0;
 	int componentHeight = 0;
 	
-	public BoardView(Dimension in)
+	Board board;
+	
+	public BoardView(Board _board, Dimension in)
 	{
+		board = _board;
 		if(in.width > in.height)
 			side = in.height;
 		else
 			side = in.width;
+		BOARDSIZE = board.BOARDSIZE;
 		
 		componentWidth = in.width;
 		componentHeight = in.height;
@@ -46,6 +59,7 @@ public class BoardView extends JComponent
 	
 	public void paint(Graphics _g)
 	{
+		/*
 		if(this.getWidth() > this.getHeight())
 			side = (int) (this.getHeight()*0.8);
 		else
@@ -53,33 +67,94 @@ public class BoardView extends JComponent
 		
 		componentWidth = this.getHeight();
 		componentHeight = this.getHeight();
-		
+		*/
 		Graphics2D g = (Graphics2D) _g;
+        
+		// Inner Player Lines
+        g.setStroke(new BasicStroke(2.0f));
         g.setColor(colourPlayer1);
-        // 		   x1             y1 			     x2               y2
-        //g.drawLine(componentWidth/5, 25, ((componentWidth-50)/5)+side, 25);
-        //g.drawLine(25, 25+(componentWidth - side)/2, 25+side, 25+(componentWidth - side)/2);
-        //g.drawLine(25, componentWidth-(componentWidth - side)/2, 25+side, componentWidth-(componentWidth - side)/2);
+        g.drawLine(startingOffsetX/2, startingOffsetY+RADIUS+RADIUS/2, startingOffsetX + (2*RADIUS*BOARDSIZE), startingOffsetY+RADIUS+RADIUS/2);
+        g.drawLine(startingOffsetX/2, 2*RADIUS*BOARDSIZE, startingOffsetX + (2*RADIUS*BOARDSIZE), 2*RADIUS*BOARDSIZE);
         g.setColor(colourPlayer2);
-        //g.drawLine(25+(componentHeight - side)/2, 25, 25+(componentHeight - side)/2, 25+side);
-        //g.drawLine(575, 0, 575, 600);
+        g.drawLine(startingOffsetX+RADIUS+RADIUS/2, startingOffsetY/2, startingOffsetX+RADIUS+RADIUS/2, startingOffsetY + (2*RADIUS*BOARDSIZE));
+        g.drawLine(2*RADIUS*BOARDSIZE, startingOffsetY/2, 2*RADIUS*BOARDSIZE, startingOffsetY + (2*RADIUS*BOARDSIZE));
+
+        // Border Lines
+        g.setStroke(new BasicStroke(3.0f));
+		g.setColor(colourBorder);
+		g.drawLine(startingOffsetX/2, startingOffsetX/2, startingOffsetX + (2*RADIUS*BOARDSIZE), startingOffsetY/2); 			// Top
+        g.drawLine(startingOffsetX/2, 2*RADIUS*BOARDSIZE+startingOffsetY, startingOffsetX + (2*RADIUS*BOARDSIZE), 2*RADIUS*BOARDSIZE+startingOffsetY);
+        g.drawLine(startingOffsetX/2, startingOffsetY/2, startingOffsetX/2, startingOffsetY + (2*RADIUS*BOARDSIZE));
+        g.drawLine(startingOffsetX + (2*RADIUS*BOARDSIZE), startingOffsetX/2, startingOffsetX + (2*RADIUS*BOARDSIZE), 2*RADIUS*BOARDSIZE+startingOffsetY);
+		
+        // Circle Outline
+        g.setStroke(new BasicStroke(0.0f));
         g.setColor(colourDefault);
         
         offsetX = startingOffsetX;
         offsetY = startingOffsetY;
         
-        for(int i=0; i < 24; i++)
+        for(int i=0; i < BOARDSIZE; i++)
         {
-        	for(int j=0; j < 24; j++)
+        	for(int j=0; j < BOARDSIZE; j++)
         	{
-        		g.drawOval(offsetX + i*10, offsetY + j*10, RADIUS, RADIUS);
-            	offsetY += 10;
+        		g.drawOval(offsetX + i*SPACE, offsetY + j*SPACE, RADIUS, RADIUS);
+            	offsetY += SPACE;
         	}
         	offsetY = startingOffsetY;
 
-        	offsetX += 10;
+        	offsetX += SPACE;
         }
 
+        // Draw Towers on the board
+        for (int i = 0; i < BOARDSIZE; i++)
+        {
+            for (int j = 0; j < BOARDSIZE; j++)
+            {
+		        if (board.getTower(i, j) != null)
+		        {
+		        	if (board.getTower(i, j).getPlayerId() == 1)
+		        	{
+		        		g.setColor(colourPlayer1);
+		        		g.fillOval((SPACE*i) + startingOffsetX + i*SPACE, (SPACE*j) + startingOffsetY + j*SPACE, RADIUS, RADIUS);
+		        	}
+		            else if (board.getTower(i, j).getPlayerId() == 2)
+		            {
+		        		g.setColor(colourPlayer2);
+		        		g.fillOval((SPACE*i) + startingOffsetX + i*SPACE, (SPACE*j) + startingOffsetY + j*SPACE, RADIUS, RADIUS);
+		            }
+		        }
+		        else
+		        {
+		        	System.out.println("No tower here to print.");
+		       	}
+	        }
+        }
+       
+        // Draw Bridges on the board
+        Bridge temp = null;
+        Tower start = null;
+        Tower end = null;
+    	int x1 = 0;
+    	int y1 = 0;
+    	int x2 = 0;
+    	int y2 = 0;
+        g.setStroke(new BasicStroke(2.5f));
+    	
+        for(int i = 0; i < board.getBridgeList().size(); i++)
+        {
+        	temp = board.getBridge(i);
+        	start = temp.getStart();
+        	end = temp.getEnd();
+        	
+        	if(test.getDebugModeOn() == true) System.out.println("Attempting to place bridge " + start.getX()+","+start.getY() + " " + end.getX()+","+end.getY());
+        	g.setColor((temp.getID() == 1) ? colourPlayer1 : colourPlayer2); // If ID is 1 then use player 1's colour, else use player 2's colour.
+        	x1 = startingOffsetX + RADIUS/2 + start.getX()*(SPACE+RADIUS);
+        	y1 = startingOffsetY + RADIUS/2 + start.getY()*(SPACE+RADIUS);
+        	x2 = startingOffsetX + RADIUS/2 + end.getX()*(SPACE+RADIUS);
+        	y2 = startingOffsetY + RADIUS/2 + end.getY()*(SPACE+RADIUS);
+        	g.drawLine(x1, y1, x2, y2);
+        }
+        	
 	}
-
 }
