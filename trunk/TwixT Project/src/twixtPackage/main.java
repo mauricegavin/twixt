cfbda92349dc6bd644f665b551c5ac15e1dc0cc2
gavinm1@tcd.ie <gavinm1@tcd.ie@7fb19570-3518-8153-c07a-fda716b8aad6>
2011-03-25 15:55:52 +0000

    Merger

--- trunk/TwixT Project/src/twixtPackage/main.java
+++ trunk/TwixT Project/src/twixtPackage/main.java
@@ -3,8 +3,28 @@ package twixtPackage;
 public class main {
 
 	public static void main(String args[])
-	{	
-		Interface mainInterface = new Interface();
+	{
+		//BoardView board = new BoardView();
+		//GameView gameInterface = new GameView(new Board());
+		//SetupView setupInterface = new SetupView();
+		
+		Game game = new Game();
+		// Tests for drawing on the board through the rule master
+		/*
+		game.placeTower(1, 1, 1);
+		game.endTurn(1);
+		game.placeTower(12, 14, 2);
+		game.endTurn(2);
+		game.placeTower(2, 3, 1);
+        game.placeBridge(1, 1, 2, 3, 1);
+		game.endTurn(1);
+		game.placeTower(14, 13, 2);
+		game.placeBridge(12, 14, 14, 13, 2);
+		game.endTurn(2);
+		*/
+		
+		
+
 		
 	}
 }
