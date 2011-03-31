549619f82160daffd2b883797efd41c826daa565
gavinm1@tcd.ie <gavinm1@tcd.ie@7fb19570-3518-8153-c07a-fda716b8aad6>
2011-03-31 13:44:19 +0000

    ResourceManager Class: -Removed null pointer exception when determining OS type. -Needs to be tested on other OS (i.e. Mac and Unix)

--- TwixT Project/src/twixtPackage/ResourceManager.java
+++ TwixT Project/src/twixtPackage/ResourceManager.java
@@ -10,17 +10,19 @@ import javax.sound.sampled.Clip;
 
 public class ResourceManager {
 
+	private static OSValidator os;
 	private PlayMedia fx;
-	private static OSValidator os; 
+	
 	ResourceManager()
 	{
+		os = new OSValidator();
+		
 		try {
 			fx = new PlayMedia();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		os = new OSValidator();
 	}
 	
 	PlayMedia getAudioObject()
@@ -31,19 +33,20 @@ public class ResourceManager {
 	static class PlayMedia
 	{
 		// SET UP
-	     private File towerFile;
-	     private File bridgeFile;
-	     private AudioInputStream stream;
-	     static Clip music;	// Clip music must be static. It is the sound file that will be played.
-	     // If we later want to stop() that sound file we need to be able to manipulate that instance of Clip.
-	     // If it is not static we have no way of retrieving it.
+	    private File towerFile;
+	    private File bridgeFile;
+	    private AudioInputStream stream;
+	    static Clip music;	// Clip music must be static. It is the sound file that will be played.
+	    // If we later want to stop() that sound file we need to be able to manipulate that instance of Clip.
+	    // If it is not static we have no way of retrieving it.
 	     
-	     /* NOTE: "throws Exception" is important for setup. Your program will NOT compile if
-	      * this is not included. */
-	     public PlayMedia() throws Exception
-	     {
-	         // FILE - part of set-up
-	    	if(os.isWindows())
+	    /* NOTE: "throws Exception" is important for setup. Your program will NOT compile if
+	     * this is not included. */
+	    public PlayMedia() throws Exception
+	    {
+
+	        // FILE - part of set-up
+	    	if(os.windows)
 	    	{
 	    		towerFile = new File("src\\twixtPackage\\media\\placetower_succeed.wav");
 	    		bridgeFile = new File("src\\twixtPackage\\media\\placebridge_succeed.wav");
@@ -59,9 +62,9 @@ public class ResourceManager {
 	    		bridgeFile = new File("src/twixtPackage/media/placebridge_succeed.wav");
 	    	}
 	     }
-	     
 	     /* NOTE: "throws Exception" is important for starting. Your program will NOT compile if
 	      * this is not included. */
+	     
 	     public void play(int control) throws Exception 
 	     {
 	          // SET UP
@@ -88,7 +91,16 @@ public class ResourceManager {
 	}
 	
 	private class OSValidator{
-		 
+		
+		public boolean windows;
+		public boolean mac;
+		public boolean unix;
+		
+		OSValidator()
+		{
+			windows = isWindows();
+		}
+		
 		private boolean isWindows(){
 			String os = System.getProperty("os.name").toLowerCase();
 			//windows
